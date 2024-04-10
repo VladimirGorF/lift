ca57ffd11d1ef4181ac2b161a6cca6e5e77411b4
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2024-04-10 21:22:33 +0400

    лифт ездит и вверх и вниз

--- src/components/MyLift.vue
+++ src/components/MyLift.vue
@@ -5,28 +5,50 @@ let liftsBox = ref([7, 6, 5, 4, 3, 2, 1]); //  список этажей
 let gapOfFloor = ref(100 / liftsBox.value.length); // сколько процентов занимает каждый этаж
 let diffGap = ref(gapOfFloor.value / 10); // пилим на 10 частей шаг лифта по 100мс
 let liftLevel = ref(String(liftsBox.value[1] * gapOfFloor.value) + "%"); // начинаем с 1 этажа
-let liftMemory = ref(1) //  не меняется в отличие liftCall при вызове лифта, а только после отработки функции
+let liftMemory = ref(1); //  память текущего этажа перед вызовом
 
-let liftCall = ref(1); // вызванный этаж
-
-function liftTimer() {
-  let counter = 1; // счетчик для интервалки 10 по 100
-  let liftFlor = liftsBox.value[liftMemory.value] * gapOfFloor.value ;
-  const timer = setInterval(function () {
-    if (counter === 10) {
-      clearInterval(timer);
-      liftLevel.value = String(liftsBox.value[liftCall.value] * gapOfFloor.value) + "%"  // при заврешении инетервалки устанавливаем уровень этажа
-      liftMemory.value += 1
+function liftTimer(liftCall) {
+  // проверяем в какую сторону двигается лифт: вверх или вниз
+  if (liftCall > liftMemory.value) {
+    let counter = 1; // счетчик для интервалки 10 по 100
+    let liftFlor = liftsBox.value[liftMemory.value] * gapOfFloor.value;
+    const timer = setInterval(function () {
+      liftLevel.value = String(liftFlor - diffGap.value) + "%";
+      counter++;
+      liftFlor -= diffGap.value; // уменьшаем отступ сверху на 1\10 от этажа
+      if (counter === (liftCall - liftMemory.value) * 10) {
+        clearInterval(timer);
+        // если это последний этаж
+        if (liftCall === liftsBox.value.length) {
+          liftLevel.value = "0%";
+        } else {
+          liftLevel.value =
+            String(liftsBox.value[liftCall] * gapOfFloor.value) + "%"; // при заврешении инетервалки устанавливаем уровень этажа
+          liftMemory.value = liftCall; // увеличиваем этаж на текущий
+        }
+        liftMemory.value = liftCall; // увеличиваем этаж на текущий
+      }
+    }, 100);
+  } else if (liftCall < liftMemory.value) {
+    let counter = 1; // счетчик для интервалки 10 по 100
+    let liftFlor = 0;
+    if (liftMemory.value === liftsBox.value.length) {
+      liftFlor = 0;
+    } else {
+      liftFlor = liftsBox.value[liftMemory.value] * gapOfFloor.value;
     }
-    liftLevel.value = String(liftFlor - diffGap.value) + "%"; 
-    counter++;
-    liftFlor -= diffGap.value; // уменьшаем отступ сверху на 1\10 от этажа
-  }, 100);
-}
-
-function goLift(liftPlace) {
-  liftCall.value = liftPlace;
-  liftTimer();
+    const timer = setInterval(function () {
+      if (counter === (liftMemory.value - liftCall) * 10) {
+        clearInterval(timer);
+        liftLevel.value =
+          String(liftsBox.value[liftCall] * gapOfFloor.value) + "%"; // при завершении инетервалки устанавливаем уровень этажа
+        liftMemory.value = liftCall; // запоминаем этаж на текущий
+      }
+      liftLevel.value = String(liftFlor + diffGap.value) + "%";
+      counter++;
+      liftFlor += diffGap.value; // уменьшаем отступ сверху на 1\10 от этажа
+    }, 100);
+  }
 }
 </script>
 
@@ -39,9 +61,9 @@ function goLift(liftPlace) {
       <div class="lift" :style="`top: ${liftLevel};`"></div>
     </div>
     <div class="containerButtons">
-      <div v-for="liftPlace in liftsBox" :key="liftPlace" class="buttonBox">
-        <div class="button" @click="goLift(liftPlace)">
-          {{ liftPlace }}
+      <div v-for="liftCall in liftsBox" :key="liftCall" class="buttonBox">
+        <div class="button" @click="liftTimer(liftCall)">
+          {{ liftCall }}
         </div>
       </div>
     </div>
@@ -65,6 +87,7 @@ function goLift(liftPlace) {
 }
 .liftPlace {
   height: 80px;
+  border: 1px dotted blue;
   width: 98%;
   display: flex;
   justify-content: center;
