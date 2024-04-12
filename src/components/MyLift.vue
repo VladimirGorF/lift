00d25292995d25a268fdcecb5556cc5c45c4e38c
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2024-04-12 15:17:09 +0400

    константы запакованы

--- src/components/MyLift.vue
+++ src/components/MyLift.vue
@@ -1,14 +1,25 @@
 <script setup>
-import { ref } from "vue";
-
-let liftsBox = ref([7, 6, 5, 4, 3, 2, 1]); //  список этажей
-let gapOfFloor = ref(100 / liftsBox.value.length); // сколько процентов занимает каждый этаж
-let diffGap = ref(gapOfFloor.value / 100); // пилим на 10 частей шаг лифта по 100мс
+import { ref, computed } from "vue";
+import {liftBox, stepFrequency, restTiming} from '../utils/index.js'
+ 
+const liftsBox = ref(liftBox);
+let gapOfFloor = ref(100 / liftsBox.value.length); // сколько процентов шахты занимает каждый этаж
+let diffGap = gapOfFloor.value / stepFrequency; // пилим на 100 частей шаг лифта по 100мс
 let liftLevel = ref(String(liftsBox.value[1] * gapOfFloor.value) + "%"); // начинаем с 1 этажа
 let liftMemory = ref(1); //  память текущего этажа перед вызовом
-let liftIsWorking = ref(false);
-let liftIsRest = ref(false);
+let liftIsWorking = ref(false); // лифт в работе
+let liftIsRest = ref(false); // лифт на отдыхе 3 с
 const queue = ref([]); // очередь вызовов
+
+const directionOfMoving = computed(() => {
+  // индикатор направления движения движения лифта
+  if (liftMemory.value < queue.value[0] && liftIsWorking.value) {
+    return queue.value[0] + " up";
+  } else if (liftMemory.value > queue.value[0] && liftIsWorking.value) {
+    return queue.value[0] + " down";
+  }
+  return null;
+});
 
 function haveRest() {
   liftIsRest.value = true; // лифт на отдыхе
@@ -20,13 +31,21 @@ function haveRest() {
       // если есть что еще отрабатывать, запускаем Лифт
       liftManager();
     }
-  }, 3000);
+  }, restTiming);
+}
+
+function liftIsComing(liftCall) {
+  // проверка на идущий лифт в направлении текущей нажатой кнопки
+  if (liftIsWorking.value && liftCall === queue.value[0]) {
+    return true;
+  }
+  return false;
 }
 
 function liftManager() {
   // переводим флаг Лифта в рабочее состояние
   liftIsWorking.value = true;
-  // берем первый в очереди этаж, отрабатываем и удаляем его из списка
+  // берем первый в очереди этаж и запускам движок лифта
   liftEngine(queue.value[0]);
 }
 
@@ -34,14 +53,18 @@ function queueLoader(liftCall) {
   // проверяем нет ли вызова уже в очереди
   if (!queue.value.includes(liftCall) && liftMemory.value !== liftCall) {
     // добавляем вызов в очередь
-    console.log("в ечередб добваляем");
     queue.value.push(liftCall);
     // запускаем менеджер, если лифт не в состоянии работы
     if (!liftIsWorking.value) {
       liftManager();
     }
-  } else if (liftCall === liftMemory.value && liftIsWorking.value) {
+  } else if (
     // проверка на вызов с этажа откуда только что уехал лифт
+    liftCall === liftMemory.value &&
+    liftIsWorking.value &&
+    !queue.value.includes(liftCall) &&
+    !liftIsRest.value
+  ) {
     queue.value.push(liftCall);
   }
 }
@@ -52,10 +75,10 @@ function liftEngine(liftCall) {
     let counter = 1; // счетчик для интервалки 10 по 100
     let liftFlor = liftsBox.value[liftMemory.value] * gapOfFloor.value;
     const timer = setInterval(function () {
-      liftLevel.value = String(liftFlor - diffGap.value) + "%";
+      liftLevel.value = String(liftFlor - diffGap) + "%";
       counter++;
-      liftFlor -= diffGap.value; // уменьшаем отступ сверху на 1\10 от этажа
-      if (counter === (liftCall - liftMemory.value) * 100) {
+      liftFlor -= diffGap; // уменьшаем отступ сверху на 1\10 от этажа
+      if (counter === (liftCall - liftMemory.value) * stepFrequency) {
         haveRest(); // моргание лифта
         clearInterval(timer);
         // если это последний этаж
@@ -77,16 +100,16 @@ function liftEngine(liftCall) {
       liftFlor = liftsBox.value[liftMemory.value] * gapOfFloor.value;
     }
     const timer = setInterval(function () {
-      if (counter === (liftMemory.value - liftCall) * 100) {
+      if (counter === (liftMemory.value - liftCall) * stepFrequency) {
         haveRest(); // моргание лифта
         clearInterval(timer);
         liftLevel.value =
           String(liftsBox.value[liftCall] * gapOfFloor.value) + "%"; // при завершении инетервалки устанавливаем уровень этажа
         liftMemory.value = liftCall; // запоминаем этаж на текущий
       }
-      liftLevel.value = String(liftFlor + diffGap.value) + "%";
+      liftLevel.value = String(liftFlor + diffGap) + "%";
       counter++;
-      liftFlor += diffGap.value; // уменьшаем отступ сверху на 1\10 от этажа
+      liftFlor += diffGap; // уменьшаем отступ сверху на 1\10 от этажа
     }, 10);
   }
 }
@@ -95,25 +118,35 @@ function liftEngine(liftCall) {
 <template>
   <div class="container">
     <div class="containerLift">
-      <div v-for="liftPlace in liftsBox" :key="liftPlace" class="liftPlace">
-        {{ liftPlace }}
-      </div>
+      <div
+        v-for="liftPlace in liftsBox"
+        :key="liftPlace"
+        class="liftPlace"
+      ></div>
       <div
         :class="['lift', { blink: liftIsRest }]"
         :style="`top: ${liftLevel};`"
-      ></div>
+      >
+        <div class="indicator" v-if="liftIsWorking && !liftIsRest">
+          {{ directionOfMoving }}
+        </div>
+        <div v-if="!liftIsWorking">{{ liftMemory }}</div>
+      </div>
     </div>
     <div class="containerButtons">
       <div v-for="liftCall in liftsBox" :key="liftCall" class="buttonBox">
         <div
-          :class="['button', { colored: queue.includes(liftCall) }]"
+          :class="[
+            'button',
+            { colored: queue.includes(liftCall) },
+            { blink: liftIsComing(liftCall) },
+          ]"
           @click="queueLoader(liftCall)"
         >
           {{ liftCall }}
         </div>
       </div>
     </div>
-    <div>{{ queue }} {{ liftMemory }}</div>
   </div>
 </template>
 
@@ -155,18 +188,21 @@ function liftEngine(liftCall) {
   position: absolute;
   top: 80%;
   left: 4px;
-  border: 2px solid brown;
+  border: 2px solid rgb(134, 12, 61);
+  border-radius: 10px;
   height: 70px;
   width: 70px;
   margin-top: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2px;
 }
-
 .blink {
   transition: 0.3s;
-  animation: blink-animation 1s steps(5, start) 3;
-  -webkit-animation: blink-animation 1s steps(5, start) 3;
+  animation: blink-animation 1s steps(5, start) infinite;
+  -webkit-animation: blink-animation 1s steps(5, start) infinite;
 }
-
 .buttonBox {
   height: 80px;
   width: 50px;
@@ -189,7 +225,8 @@ function liftEngine(liftCall) {
   background-color: red;
 }
 .colored {
-  background-color: blue;
+  background-color: rgb(72, 163, 33);
   transition: 0.3s;
+  color: aliceblue;
 }
 </style>
---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+// конастанты управления конструктором лифта
+export const stepFrequency = 100; // скорость движения лифта 100ед = 1эт/сек
+export const restTiming = 3000; // время отдыха лифт в мс
+const numberOfFloors = 5; // количество этажей лифта
+
+export const liftBox = []; //  список этажей
+
+function liftConstructor(numberOfFloors) {
+  for (let i = 1; i <= numberOfFloors; i++) {
+    liftBox.unshift(i);
+  }
+}
+
+liftConstructor(numberOfFloors); // вызываем конструктор шахты лифта
